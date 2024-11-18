7f100fd0915fb3eb4c042cd3af9700d3294d25d2
user <nilov@neo-s.com>
2024-11-19 00:06:45 +0300

    фф

--- src/main/java/springcourse/controllers/PeopleController.java
+++ src/main/java/springcourse/controllers/PeopleController.java
@@ -3,10 +3,12 @@ package springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import springcourse.dao.PersonDAO;
+import springcourse.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -31,4 +33,44 @@ public class PeopleController {
         return "people/show";
     }
 
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult)  {
+        if (bindingResult.hasErrors()) {
+            return "people/new";
+        }
+
+        personDAO.save(person);
+        return "redirect:/people";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors()) {
+            return "people/edit";
+        }
+
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@ModelAttribute("id") int id) {
+        personDAO.delete(id);
+        return "redirect:/people";
+    }
 }
--- src/main/java/springcourse/dao/PersonDAO.java
+++ src/main/java/springcourse/dao/PersonDAO.java
@@ -15,10 +15,10 @@ public class PersonDAO {
     {
         people = new ArrayList<Person>();
 
-        people.add(new Person(++PEOPLE_COUNT, "Tom"));
-        people.add(new Person(++PEOPLE_COUNT, "Jack"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob"));
-        people.add(new Person(++PEOPLE_COUNT, "Kate"));
+        people.add(new Person(++PEOPLE_COUNT, "Tom", 20, "tom@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Jack", 23, "jack@mail.ru"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob", 33, "bob@mail"));
+        people.add(new Person(++PEOPLE_COUNT, "Kate", 22, "kate@mail"));
     }
 
     public List<Person> index() {
@@ -27,5 +27,22 @@ public class PersonDAO {
 
     public Person show(int id) {
         return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatePerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatePerson.getName());
+        personToBeUpdated.setAge(updatePerson.getAge());
+        personToBeUpdated.setEmail(updatePerson.getEmail());
+    }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
     }
 }
